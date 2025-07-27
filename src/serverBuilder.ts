70318e9f27339c3757c9f1f8c90d4e2d9ffc1526
Niv Greenstein <88280771+NivGreenstein@users.noreply.github.com>
2025-07-27 17:04:52 +0300

    chore: removed unused code

--- src/serverBuilder.ts
+++ src/serverBuilder.ts
@@ -1,21 +1,15 @@
 import express from 'express';
 import { inject, injectable } from 'tsyringe';
-import type { Logger } from '@map-colonies/js-logger';
 import { getTraceContexHeaderMiddleware } from '@map-colonies/telemetry';
 import { collectMetricsExpressMiddleware } from '@map-colonies/telemetry/prom-metrics';
 import { Registry } from 'prom-client';
-import type { ConfigType } from '@common/config';
 import { SERVICES } from '@common/constants';
 
 @injectable()
 export class ServerBuilder {
   private readonly serverInstance: express.Application;
 
-  public constructor(
-    @inject(SERVICES.CONFIG) private readonly config: ConfigType,
-    @inject(SERVICES.LOGGER) private readonly logger: Logger,
-    @inject(SERVICES.METRICS) private readonly metricsRegistry: Registry
-  ) {
+  public constructor(@inject(SERVICES.METRICS) private readonly metricsRegistry: Registry) {
     this.serverInstance = express();
   }
 
